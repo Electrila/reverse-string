3b767b0efb4808256ceb510dc638be591d23ef7e
megann <megann.elizabeth.king@gmail.com>
2022-01-07 19:20:21 -0500

    commit tree 14c8b304ca9393127686ff7048ded68a0ec68517

--- ruby/reverse_string.rb
+++ ruby/reverse_string.rb
@@ -1,6 +1,14 @@
 def reverse_string(str)
-  # type your code in here
+  result = []
+  str_arr = str.split("")
+
+  while str_arr.length > 0
+    letter = str_arr.pop
+    result << letter
+  end
+  result.join
 end
+
 
 if __FILE__ == $PROGRAM_NAME
   puts "Expecting: 'ih'"
@@ -14,5 +22,13 @@ if __FILE__ == $PROGRAM_NAME
   # Don't forget to add your own!
 end
 
+
 # Please add your pseudocode to this file
 # And a written explanation of your solution
+=begin
+1. Create empty result array
+2. Convert str to array
+3. Create while loop that iterates through each str char and uses #pop to remove last char to store in temp variable
+4. Push each iteration of temp variable to result array
+5. Join result array to convert back into results string
+=end
